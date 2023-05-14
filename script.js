b13bc466f8d93b90fb74171ffcf102bb2583811b
Elena Kuzmitskaya <e.kuzmitskaya@gmail.com>
2023-05-15 02:55:43 +0300

    Рабочий вариант доработать удаление сообщения о повторах

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //Коллекция узлов
 let list = document.querySelector(".todo-list");
 let items = list.children;
-console.log (items)
-//Пустая задача
+console.log(items);
+//Высплывающие подсказки
 let emptyListMessage = document.querySelector(".empty-tasks");
+let existedTask = document.querySelector(".task-exists");
 //Элементы формы, для добавления задач
 let newItemForm = document.querySelector(".add-form");
 let newItemTitle = newItemForm.querySelector(".add-form-input");
@@ -11,11 +12,16 @@ let newItemTitle = newItemForm.querySelector(".add-form-input");
 let taskTemplate = document.querySelector("#task-template").content;
 let newItemTemplate = taskTemplate.querySelector(".todo-list-item");
 //Разделы
-let activeSection = document.querySelector('.active-tasks');
-let activeList = document.querySelector(".active-todo");
-let activeItems = activeList.children;
-let doneSection = document.querySelector('.finished-tasks');
+let activeSection = document.querySelector(".active-tasks");
+let activeItems = activeSection.children;
+let doneSection = document.querySelector(".finished-tasks");
 
+//Счетчик задач
+let counter = document.getElementById("counter");
+let setCounter = function () {
+  counter.textContent = activeItems.length;
+};
+setCounter();
 
 //Сообщение о выполнении всех задач
 let toggleEmptyListMessage = function () {
@@ -26,77 +32,28 @@ let toggleEmptyListMessage = function () {
   }
 };
 
-//TODO Перенос задачи в выполненное
-//Обработчик нажатия на чекбокс
-document.querySelector('.active-tasks').onclick = function(e) {
-  if(e.target.value) {
+// Перенос задачи в выполненное
+document.querySelector(".active-tasks").onclick = function (e) {
+  if (e.target.value) {
     doneSection.appendChild(e.target.parentNode.parentNode);
     e.target.parentNode.parentNode.classList.remove("active-todo");
     e.target.parentNode.parentNode.classList.add("finished-task");
     e.target.remove();
+    setCounter();
     toggleEmptyListMessage();
   }
-}
-
-//TODO Удаление задачи
-/* ---------------- Рабочий вариант с обработчиком на всем контейнере
-const container = document.querySelector('.container')
-
-container.addEventListener('click', (e) => {
-   const isRemoveButton = e.target.classList.contains('delete-button')
-
-   if (isRemoveButton) {
-      console.log ('deleted')
-   }
-}) 
-
-*/
-
-
-
-let removeButton = document.getElementsByClassName('delete-button');
-let deleteItem = function (item) {
-  let buttons = item.querySelector(".delete-button");
-  buttons.addEventListener("click", function () {
-    console.log ('deleted')
-  });
 };
 
-for (let i = 0; i < removeButton.length; i++) {
-  deleteItem(removeButton[i]);
-}
-
-
-
-
-// let removeButton = document.getElementsByClassName('delete-button');
-
-// for (let i = 0; i < removeButton.length; i++) {
-//   removeButton[i].onclick = function(e) {
-//     console.log ('deleted')
-//     // e.target.parentNode.parentNode.parentNode.remove();
-//     // toggleEmptyListMessage();
-// }
-// }
-
-// removeButtonArray.forEach(item => {
-//   item.addEventListener('click', event => {
-//     console.log ('deleted')
-//   })})
-
-// let removeItem = function() {
-//   console.log ('deleted')
-// }
-// removeButton.forEach(el => el.addEventListener('click', removeItem));
-
-
-// removeButton.forEach.call((item) => {
-//   item.onclick = function(e) {
-//     console.log ('deleted')
-//     e.target.parentNode.parentNode.parentNode.remove();
-//     toggleEmptyListMessage();
-// }
-// });
+// Удаление задачи
+// Рабочий вариант с обработчиком на всем контейнере
+const container = document.querySelector(".container");
+container.addEventListener("click", (e) => {
+  const isRemoveButton = e.target.classList.contains("delete-button");
+  if (isRemoveButton) {
+    e.target.parentNode.parentNode.parentNode.remove();
+    setCounter();
+  }
+});
 
 //Добавление новой задачи
 newItemForm.addEventListener("submit", function (e) {
@@ -105,27 +62,45 @@ newItemForm.addEventListener("submit", function (e) {
   let taskText = newItemTitle.value;
   let task = newItemTemplate.cloneNode(true);
   let taskDescription = task.querySelector("span");
-  let deleteButton = newItemTemplate.querySelector('.delete-button')
+  let deleteButton = newItemTemplate.querySelector(".delete-button");
   let newdeleteButton = deleteButton.cloneNode(true);
   taskDescription.textContent = taskText;
-  
-  //Добавление важной задачи
-  let importantCheckbox = document.querySelector(".important-button");
-  if (importantCheckbox.checked === true) {
-    task.classList.add("important");
-  }
 
-  //TODO: Проверка на повторы задач - дописать поведение
+  //TODO: Проверка на повторы задач
   let tasksArray = Array.from(items).map((child) => child.outerText);
-  console.log(tasksArray);
   let arrayCheck = tasksArray.includes(taskText);
+
+  newItemTitle.addEventListener('change', () => {
+    
+  }
+ 
+  )
+
   if (arrayCheck == true) {
-    console.log("error");
+    existedTask.classList.remove("hidden");
+  } else if (newItemTitle.value == "") {
+    // TODO: убрать когда строка очищена
+    existedTask.classList.add("hidden");
   } else {
-    //Вставка в DOM
-    activeSection.appendChild(task);
-    taskDescription.appendChild(newdeleteButton);
-    toggleEmptyListMessage();
-    newItemTitle.value = "";
+    existedTask.classList.add("hidden");
+    //Добавление важной задачи
+    let importantCheckbox = document.querySelector(".important-button");
+    if (importantCheckbox.checked === true) {
+      task.classList.add("important");
+
+      //Вставка в DOM в начало
+      activeSection.insertBefore(task, activeSection.firstChild);
+      taskDescription.appendChild(newdeleteButton);
+      toggleEmptyListMessage();
+      setCounter();
+      newItemTitle.value = "";
+    } else {
+      //Вставка в DOM в конец
+      activeSection.appendChild(task);
+      taskDescription.appendChild(newdeleteButton);
+      toggleEmptyListMessage();
+      setCounter();
+      newItemTitle.value = "";
+    }
   }
 });
